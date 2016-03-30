6de7798ad3db7df7a443e952f400b09428459721
Dmitrij Rusakov <Daemon2017@gmail.com>
2016-03-30 23:50:43 +0300

    1st

---
+++ FuncMin/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace Minimization
+{
+    partial class Minimization
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 62);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Рассчет";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(194, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(129, 37);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Результат";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(103, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(20, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Интервал:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(77, 6);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(20, 20);
+            this.textBox2.TabIndex = 10;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(195, 8);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(171, 17);
+            this.checkBox1.TabIndex = 12;
+            this.checkBox1.Text = "Метод половинного деления";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(372, 8);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(119, 17);
+            this.checkBox2.TabIndex = 13;
+            this.checkBox2.Text = "Метод Фибонначи";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(497, 8);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(151, 17);
+            this.checkBox3.TabIndex = 14;
+            this.checkBox3.Text = "Метод золотого сечения";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(369, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(494, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 37);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Точность:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(75, 34);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(48, 20);
+            this.textBox3.TabIndex = 18;
+            this.textBox3.Text = "1e-3";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 88);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(79, 13);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "f(x)=7x^2-10x-4";
+            // 
+            // Minimization
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(645, 111);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Minimization";
+            this.Text = "Поиск min функции на участке";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ FuncMin/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Minimization
+{
+    public partial class Minimization : Form
+    {
+        public Minimization()
+        {
+            InitializeComponent();
+        }
+
+        double X, Y;
+        double a, b;
+        double acc;
+
+        void metPolDil()
+        {
+            getValues();
+            double x = 0;
+            while ((b - a) >= acc)
+            {
+                x = (b + a) / 2;
+                X = x - acc / 2;
+                Y = x + acc / 2;
+                if (f(X) >= f(Y))
+                {
+                    a = x;
+                }
+                else
+                {
+                    b = x;
+                }
+                x = b - a;
+            }
+            double res = ((b + a) / 2);
+            label1.Text = res.ToString();
+        }
+
+        void metZolSech()
+        {
+            getValues();
+            double gold = (Math.Sqrt(5) - 1) / 2;
+            double c = b - gold * (b - a);
+            double d = a + gold * (b - a);
+            while (Math.Abs(c - d) > acc)
+            {
+                double fc = f(c);
+                double fd = f(d);
+                if (fc < fd)
+                {
+                    b = d;
+                    d = c;
+                    c = b - gold * (b - a);
+                }
+                else
+                {
+                    a = c;
+                    c = d;
+                    d = a + gold * (b - a);
+                }
+            }
+            double res = ((b + a) / 2);
+            label2.Text = res.ToString();
+        }
+
+        void metFib()
+        {
+            getValues();
+            double[] Fibo = new double[] { 0, 1, 1 };
+            double calcNum;
+            double Y1, Y2;
+            int N = 3;
+            calcNum = (b - a) / (2 * acc);
+            while (Fibo[2] < calcNum)
+            {
+                N++;
+                Fibo[0] = Fibo[1];
+                Fibo[1] = Fibo[2];
+                Fibo[2] = Fibo[1] + Fibo[0];
+            }
+            X = (b - a) * Fibo[0] / Fibo[2] + a;
+            Y = (b - a) * Fibo[1] / Fibo[2] + a;
+            while (N > 1)
+            {
+                Y1 = f(X);
+                Y2 = f(Y);
+                if (Y1 <= Y2)
+                {
+                    b = Y;
+                    Y = X;
+                    X = a + Fibo[0] * (b - a) / Fibo[2];
+                }
+                else
+                {
+                    a = X;
+                    X = Y;
+                    Y = a + Fibo[1] * (b - a) / Fibo[2];
+                }
+                Fibo[2] = Fibo[1];
+                Fibo[1] = Fibo[0];
+                Fibo[0] = Fibo[2] - Fibo[1];
+                N--;
+            }
+            double res = ((b + a) / 2);
+            label3.Text = res.ToString();
+        }
+
+        double f(double x)
+        {
+            return 7 * Math.Pow(x, 2) - 10 * Math.Pow(x, 1) - 4;
+        }
+
+        double choose(double i)
+        {
+            if (i < 0)
+            {
+                return -i;
+            }
+            else
+            {
+                return i;
+            }
+        }
+
+        void getValues()
+        {
+            acc = Convert.ToDouble(textBox3.Text);
+            b = Convert.ToDouble(textBox1.Text);
+            a = Convert.ToDouble(textBox2.Text);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                metPolDil();
+            }
+            if (checkBox2.Checked == true)
+            {
+                metFib();
+            }
+            if (checkBox3.Checked == true)
+            {
+                metZolSech();
+            }
+        }
+    }
+}
